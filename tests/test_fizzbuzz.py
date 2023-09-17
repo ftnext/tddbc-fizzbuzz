2b314bfb6ff5a873f8b011bcfeee8efe68165c06
ftnext <takuyafjp+develop@gmail.com>
2023-09-17 18:39:42 +0900

    [refactor] 1周目リファクタリング：変数のインライン化

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -4,5 +4,4 @@ from fizzbuzz import FizzBuzz
 class TestFizzBuzz:
     def test_1を渡すと文字列1を返す(self):
         fizzbuzz = FizzBuzz()
-        actual = fizzbuzz.convert(1)
-        assert "1" == actual
+        assert "1" == fizzbuzz.convert(1)
