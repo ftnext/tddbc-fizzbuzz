398ac5bc95536aeb09327be603b193f1dd1c147e
ftnext <takuyafjp+develop@gmail.com>
2023-09-17 19:15:06 +0900

    [test] 3周目: 3のケースを追加

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -9,3 +9,7 @@ class TestFizzBuzz:
     def test_2を渡すと文字列2を返す(self):
         fizzBuzz = FizzBuzz()
         assert "2" == fizzBuzz.convert(2)
+
+    def test_3を渡すと文字列Fizzを返す(self):
+        fizzBuzz = FizzBuzz()
+        assert "Fizz" == fizzBuzz.convert(3)
