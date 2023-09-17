9ddb4fd18004ecb7b7dc3fa08118c3feab426c34
ftnext <takuyafjp+develop@gmail.com>
2023-09-17 19:26:57 +0900

    [refactor] 3周目: 前準備を共通化

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -2,14 +2,14 @@ from fizzbuzz import FizzBuzz
 
 
 class TestFizzBuzz:
+    def setup_method(self):
+        self.fizzBuzz = FizzBuzz()
+
     def test_1を渡すと文字列1を返す(self):
-        fizzbuzz = FizzBuzz()
-        assert "1" == fizzbuzz.convert(1)
+        assert "1" == self.fizzBuzz.convert(1)
 
     def test_2を渡すと文字列2を返す(self):
-        fizzBuzz = FizzBuzz()
-        assert "2" == fizzBuzz.convert(2)
+        assert "2" == self.fizzBuzz.convert(2)
 
     def test_3を渡すと文字列Fizzを返す(self):
-        fizzBuzz = FizzBuzz()
-        assert "Fizz" == fizzBuzz.convert(3)
+        assert "Fizz" == self.fizzBuzz.convert(3)
