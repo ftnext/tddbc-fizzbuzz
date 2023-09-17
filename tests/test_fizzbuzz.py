5a798b28f10d5c16abce08e6a6f3faa87f879861
ftnext <takuyafjp+develop@gmail.com>
2023-09-17 18:47:25 +0900

    [test] 2周目: 2のときのテストを追加

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -5,3 +5,7 @@ class TestFizzBuzz:
     def test_1を渡すと文字列1を返す(self):
         fizzbuzz = FizzBuzz()
         assert "1" == fizzbuzz.convert(1)
+
+    def test_2を渡すと文字列2を返す(self):
+        fizzBuzz = FizzBuzz()
+        assert "2" == fizzBuzz.convert(2)
