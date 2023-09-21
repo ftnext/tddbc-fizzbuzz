79945bf40fa4157cd90b743b01b97c5242248d36
ftnext <takuyafjp+develop@gmail.com>
2023-09-21 23:28:05 +0900

    [refactor] 仕様は3つの集合に分解された

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -3,21 +3,22 @@ from fizzbuzz import FizzBuzz
 
 class TestFizzBuzz:
     class Test_数を文字列に変換する:
-        def setup_method(self):
-            self.fizzBuzz = FizzBuzz()
+        class Test_3の倍数のときは数の代わりにFizzに変換する:
+            def test_3を渡すと文字列Fizzを返す(self):
+                fizzBuzz = FizzBuzz()
+                assert "Fizz" == fizzBuzz.convert(3)
 
-        def test_1を渡すと文字列1を返す(self):
-            assert "1" == self.fizzBuzz.convert(1)
+        class Test_5の倍数のときは数の代わりにBuzzに変換する:
+            def test_5を渡すと文字列Buzzを返す(self):
+                fizzBuzz = FizzBuzz()
+                assert "Buzz" == fizzBuzz.convert(5)
 
-        def test_2を渡すと文字列2を返す(self):
-            assert "2" == self.fizzBuzz.convert(2)
+        class Test_その他の数のときはそのまま文字列に変換する:
+            def setup_method(self):
+                self.fizzBuzz = FizzBuzz()
 
-    class Test_3の倍数のときは数の代わりにFizzに変換する:
-        def test_3を渡すと文字列Fizzを返す(self):
-            fizzBuzz = FizzBuzz()
-            assert "Fizz" == fizzBuzz.convert(3)
+            def test_1を渡すと文字列1を返す(self):
+                assert "1" == self.fizzBuzz.convert(1)
 
-    class Test_5の倍数のときは数の代わりにBuzzに変換する:
-        def test_5を渡すと文字列Buzzを返す(self):
-            fizzBuzz = FizzBuzz()
-            assert "Buzz" == fizzBuzz.convert(5)
+            def test_2を渡すと文字列2を返す(self):
+                assert "2" == self.fizzBuzz.convert(2)
