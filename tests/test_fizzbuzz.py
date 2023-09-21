f2c6321353725b6d834f2e61b27441bdba46dd96
ftnext <takuyafjp+develop@gmail.com>
2023-09-21 23:18:58 +0900

    [refactor] 仕様を表現するテストに変える（3の倍数）

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -11,8 +11,10 @@ class TestFizzBuzz:
     def test_2を渡すと文字列2を返す(self):
         assert "2" == self.fizzBuzz.convert(2)
 
-    def test_3を渡すと文字列Fizzを返す(self):
-        assert "Fizz" == self.fizzBuzz.convert(3)
+    class Test_3の倍数のときは数の代わりにFizzに変換する:
+        def test_3を渡すと文字列Fizzを返す(self):
+            fizzBuzz = FizzBuzz()
+            assert "Fizz" == fizzBuzz.convert(3)
 
     def test_5を渡すと文字列Buzzを返す(self):
         assert "Buzz" == self.fizzBuzz.convert(5)
